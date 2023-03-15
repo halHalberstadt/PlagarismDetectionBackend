bcf4adfc8b1252e2782b80d42f7107be35a35f0b
halHalberstadt <halberstadtsterling@gmail.com>
2023-03-15 14:10:15 -0700

    modified:   README.md 	modified:   src/main/java/com/plagarism/detect/controllers/ScraperController.java 	deleted:    src/main/java/com/plagarism/detect/docs/pdf-file.pdf

--- src/main/java/com/plagarism/detect/controllers/ScraperController.java
+++ src/main/java/com/plagarism/detect/controllers/ScraperController.java
@@ -20,24 +20,70 @@ public class ScraperController {
     * 1. send scraper to search each query and return which queries return a
     * positive result
     * 2. compare documents
-    * 3. test hello world
+    * 3. Text document query parser
+    * 4. Word document query parser
+    * 5. info endoint
+    * 6. test hello world
+    * 7. Error catcher
     */
 
+    /* TODO
+     * getQueryResults
+     */
    @GetMapping(value = "/scraper")
    public Queries getQueryResults(@RequestBody QueriesDTO queries) {
       Queries newQueries = queriesDTOtoQueries(queries);
       Queries foundOnChegg = useScraper(newQueries);
+      // Put scraper function here
       return foundOnChegg;
    }
 
+   /*
+    * TODO
+    */
+   @GetMapping(value = "/compare")
+   public void compareDocuments(@RequestBody Object param) {
+
+   }
+
+   /*
+    * TODO
+    */
+   @GetMapping(value = "/text")
+   public Queries textDocuemntReader(@RequestBody String document) {
+      return null;
+   }
+
+   /*
+    * TODO
+    */
+   @GetMapping(value = "/word")
+   public Queries wordDocuemntReader(@RequestBody String document) {
+      return null;
+   }
+
+   /* TODO
+    * info send a text response of basic information of endpoints on the API
+    */
    @GetMapping(value = "/info")
    public String info() {
       return "Info on API: (Under Construction)";
    }
 
+   /* TODO
+    * this is he home or landing page mostly as a test to make sure the API is running.
+    */
    @GetMapping(value = "/")
    public String home() {
-      return "Hello World";
+      return "Plagarism Detection Service.";
+   }
+   
+   /* TODO
+    * This is a catch-all error page that gives a basic non-descript error report.
+    */
+   @GetMapping(value = "/error")
+   public String error() {
+      return "Sorry, an error occured";
    }
 
    /*
