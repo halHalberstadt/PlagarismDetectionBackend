500f0c1c0c081f1c1b3880325c9c641d7ba77995
halHalberstadt <halberstadtsterling@gmail.com>
2023-04-05 12:56:10 -0700

    changed to java 11 to see if that lets me deploy

--- src/main/java/com/plagarism/detect/controllers/QueryController.java
+++ src/main/java/com/plagarism/detect/controllers/QueryController.java
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
